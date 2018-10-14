315ac5b8c6cc8ddf0d009713b3ec405a9f838a55
AmeliePick <widomaker86@gmail.com>
2018-10-14 19:31:58 +0300

    Program entry module

---
+++ Amelie/entry_input.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*
+
+
+def start():
+    start = input("\n Hi, I'm Amelie. Start me? Press [Y] of [N]: ")
+
+    while (True):
+        if (start == "Y" or start == "y"):
+            print ("\n Salute! What I'm capable of, you can find out right now =D")
+            break
+        elif (start == "N" or start == "n"):
+            print ("\n Thinking... Now you are working, bye =D")
+            sys_exit()
+        else:
+            start = input("\n (!) Try again - [Y] or [N]: ")
+            continue
+
