f35b2bcac3f6bebf8f79965420bc17b4c894862c
AmeliePick <widomaker86@gmail.com>
2018-10-23 20:57:37 +0300

    Main window interface

--- Amelie/GUI/AmelieGUI.py
+++ Amelie/GUI/AmelieGUI.py
@@ -8,6 +8,8 @@ from kivy.uix.floatlayout import FloatLayout
 
 from kivy.uix.anchorlayout import AnchorLayout
 
+from kivy.uix.boxlayout import BoxLayout
+
 from kivy.properties import ListProperty
 from kivy.config import Config
 
@@ -22,19 +24,27 @@ class Header(Label):
 
 class AmelieApp(App):
     def build(self):
-        
-         
+
+
         al = AnchorLayout(anchor_x = 'center', anchor_y = 'top')
         label = Header(
+            font_name = '../Amelie/libs/fonts/Magneto-SuperBoldExtended.ttf',
             text='Amelie',
             color = [.66, .57, .71, 1],
-            font_size = 50,
+            font_size = 30,
             pos=(100, 100),
-            size_hint=(1, 0.3))
+            size_hint=(1, 0.13))
         al.add_widget(label)
 
-        
 
+        bottom = AnchorLayout(anchor_x = 'center', anchor_y = 'bottom')
+        lbl_bottom = Header(
+                color = [.66, .57, .71, 1],
+                font_size = 30,
+                pos=(100, 100),
+                size_hint=(1, 0.07))
+        bottom.add_widget(lbl_bottom)
+        al.add_widget(bottom)
         return al
 
 
