5fc9042297153077588910868dae091b6b1879bb
AmeliePick <widomaker86@gmail.com>
2018-11-17 20:59:24 +0300

    Added comments to the code

--- Amelie/Chat_AI_with_syn.py
+++ Amelie/Chat_AI_with_syn.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 
+'''
+Chat module for synthesis
+
+The function gets the result of the chat feature of Chat_AI and passes them to the function synthesis of speak() of Speak of the module
+
+'''
+
 import sys, os, random, pickle, re, playsound
 
 from Chat_AI import open_AI
