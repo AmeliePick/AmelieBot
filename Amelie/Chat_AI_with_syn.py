d0773a4ea04f5806373e15aea71032a871c115b4
AmeliePick <widomaker86@gmail.com>
2018-11-22 15:18:52 +0300

    Fixed bugs

--- Amelie/Chat_AI_with_syn.py
+++ Amelie/Chat_AI_with_syn.py
@@ -3,15 +3,22 @@
 '''
 Chat module for synthesis
 
-The function gets the result of the chat feature of Chat_AI and passes them to the function synthesis of speak() of Speak of the module
-
+The function returns the value from REG in the open_AI (for typing a question) in Ansever (for generating an answer) in SPEC (for articulating an answer)
 '''
 
 import sys, os, random, pickle, re, playsound
 
-from Chat_AI import open_AI
+from Chat_AI import Answer, open_AI
+from libs.Recognition import REG
 from libs.Speak import speak
 
 def speech():
 
-    return speak(open_AI())
+    return speak(Answer(open_AI(REG())))
+
+
+while(True):
+    speech()
+
+    if speech == 1:
+        os.system("pause")
