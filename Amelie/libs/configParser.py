58c560905ff5234e88a9bae78f0158626574f2af
AmeliePick <widomaker86@gmail.com>
2018-12-12 15:15:59 +0300

    Added function for parsing official words

--- Amelie/libs/configParser.py
+++ Amelie/libs/configParser.py
@@ -13,6 +13,7 @@ def createConfig(path, value):
     """
     Create a config file
     """
+
     config = configparser.ConfigParser()
     config.add_section("Settings")
     config.set("Settings", "Language", value)
@@ -30,3 +31,23 @@ def Config(path):
     lang = config.get("Settings", "Language")
 
     return lang
+
+
+
+def Parser(value):
+    lang = Config("./settings.ini")
+    if lang == "RU":
+        with open("../DataBase/Service_expressionsRU.json", encoding='utf-8') as file:
+            ReadFile = file.readlines()
+
+    elif Config("./settings.ini") == "EN":
+        with open("../DataBase/Service_expressionsEN.json", encoding='utf-8') as file:
+            ReadFile = file.readlines()
+        
+    text = []
+    for line in ReadFile:
+        row = line.split(' # ')
+
+        if row[0] == value:
+            text.append(row[1])
+            return ''.join(text)
