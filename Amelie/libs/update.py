23630cb010f05ce897b28348a6436f9f66270677
AmeliePick <widomaker86@gmail.com>
2019-03-26 15:23:53 +0300

    Update function

--- Amelie/libs/update.py
+++ Amelie/libs/update.py
@@ -1,20 +1,38 @@
 # -*- coding: utf-8 -*-
 from urllib.request import urlopen
-from configParser import Config
+from .configParser import Config, Parser
 from re import sub
 
 
 def checkUpdate():
-
-    response = urlopen('http://ameliepick.ml/update.ini')
-    with open ("t.ini", 'wb') as tmp:
-        tmp.write(response.read())
+    try:
+        response = urlopen('http://ameliepick.ml/update.ini')
+        with open ("t.ini", 'wb') as tmp:
+            tmp.write(response.read())
+    except:
+        return Parser("service_error")
      
 
 def download(response):
+        if(Config('t.ini', "update") == Config("update.ini", "update")):
+            R = Config('t.ini', "modules").split();
+            getModules = []
+            for i in R:
+                getModules.append(i)
+            
 
-        if(Config('t.ini', "update") == Config("../update.ini", "update")):
-            return 0
+            prefix = "modules/"
+            for i in getModules:
+                getFile = urlopen('http://ameliepick.ml/modules/'+i)
+
+                with open('tmp_file.py', 'wb') as tmp:
+                    tmp.write(getFile.read())
+
+                with open(prefix+i, 'w') as mergeFile:
+                    with open('tmp_file.py', 'r') as tmp2:
+                        mergeFile.write(tmp2.read())
+
+            return Parser("Yupdate")
 
         else:
-            return 1;
+            return Parser("Nupdate")
