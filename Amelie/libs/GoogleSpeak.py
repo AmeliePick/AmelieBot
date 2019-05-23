8e81c988ab367ff8b5304c7721d3b84e4a9aafbc
AmeliePick <widomaker86@gmail.com>
2019-05-23 15:22:52 +0300

    Work on sound reproduction

--- Amelie/libs/GoogleSpeak.py
+++ Amelie/libs/GoogleSpeak.py
@@ -7,11 +7,12 @@ The synthesis is based on gTTS (Google Text-to-Speech). Gets data from a chat fi
 Due to the algorithm of the library, there is a delay of a few seconds before playback
 '''
 
-from os         import remove
+import os
 from random     import randint
 from libs.tts   import gTTS
 from sys        import exit
-from ctypes import windll
+import pyaudio
+import wave
 
 
 
@@ -25,12 +26,26 @@ def speak(speech):
     #take name of file
     r1 = randint(1,10000000)
 
-    file = "sound"+str(r1) +".mp3"
+    file = "sound"+".mp3"
+
+    answer.save(file)
 
     #play sound
-    answer.play()
+    # TODO: Fix decoding
+    CHUNK_SIZE = 1024
+    FORMAT = pyaudio.paInt16
+    RATE = 44100
 
+    p = pyaudio.PyAudio()
+    output = p.open(format=FORMAT,
+                            channels=1,
+                            rate=RATE,
+                            output=True) # frames_per_buffer=CHUNK_SIZE
 
+    with open(file, 'rb') as fh:
+        while fh.tell() != os.stat(file).st_size : # get the file-size from the os module
+            AUDIO_FRAME = fh.read(CHUNK_SIZE)
+            output.write(AUDIO_FRAME)
 
 
     if(speech.getNum() == 0):
