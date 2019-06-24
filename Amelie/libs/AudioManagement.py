86ace7f6191f82eaff79c79722f1c15991786b61
AmeliePick <widomaker86@gmail.com>
2019-06-24 15:06:36 +0300

    Added support for audio playback in 32x systems

--- Amelie/libs/AudioManagement.py
+++ Amelie/libs/AudioManagement.py
@@ -4,10 +4,20 @@ import pyglet
 from pyglet.media import Player
 import pyglet
 
+from os import getenv
+
 
 def playAudio(sound, reps=1):
-    dll_name ='libs/avbin64.dll'
-    pyglet.lib.load_library(dll_name)
+    #TODO: REBUILD HERE
+    dir = "libs/"
+    dll_name = ""
+    
+    if ("64" in getenv('PROCESSOR_ARCHITECTURE')) or ("64" in getenv('PROCESSOR_ARCHITEW6432')): 
+        dll_name = "avbin64.dll"
+    else:
+        dll_name = "avbin.dll"
+
+    pyglet.lib.load_library(dir + dll_name)
 
     player = Player()
     song = pyglet.media.load(sound,streaming=False)
