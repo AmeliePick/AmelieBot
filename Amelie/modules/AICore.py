911d11b2b8391cbda41f7ff5f2301245e94fcb17
AmeliePick <widomaker86@gmail.com>
2019-07-04 15:39:10 +0300

    Slow loads marked

--- Amelie/modules/AICore.py
+++ Amelie/modules/AICore.py
@@ -9,10 +9,10 @@ User input is also processed for search engines. Unnecessary part of the phrase 
 
 '''
 
-from numpy          import random, arange
+from numpy          import random, arange #slow loading
 from pickle         import dump, load
 
-from sklearn.feature_extraction.text    import TfidfVectorizer
+from sklearn.feature_extraction.text    import TfidfVectorizer #slow loading
 from sklearn.linear_model               import SGDClassifier
 from sklearn.pipeline                   import Pipeline
 
