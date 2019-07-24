533810caa36882014546c5099e1f19d985d4829d
AmeliePick <widomaker86@gmail.com>
2019-07-24 13:24:47 +0300

    Fix a music playback

--- Amelie/libs/AudioManagement.py
+++ Amelie/libs/AudioManagement.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 
 import pyglet
-from time   import sleep
+from time               import sleep
+from sys                import exit
+from os                 import getenv
+from os                 import path as os_path
+from os                 import remove
+from .pyglet.player     import Player
+from .pyglet.app        import _run
 
-from os             import getenv
-from .pyglet.player  import Player
 
 
 
@@ -32,12 +36,30 @@ def playAudio(sound, reps=1):
         pyglet.app.exit()
 
     pyglet.clock.schedule_once(callback, _song.duration*reps)
-    #TODO: fix here for TTSWatson response
-    #pyglet.app.run()
-    sleep(0.5)
+    pyglet.app._run()
     
+    #TODO: delay while music is playing
 
     return player
 
-    
-    
+
+def _exit(file: str, player: Player) -> None:
+    if os_path.exists(file):
+        ''' We can use song.__del__()
+        # Delete the source of AVbin, close the file and stop the thread.
+        # Anyway it will works, because its in a source file:
+        # pyglet.media.sources.avbin.py -> line 327 -> __del__(self)
+        ================================================================
+            Or we can use player.delete() - due to the pyglet.player uses
+        # Includes a call of __del__() from pyglet.media.sources.avbin.py
+        # Deleting of all child objects of player
+        # I added a few changes due to WinError 32. Didn't remove the mp3 file, because other process used it.
+        # A new file was saved in Amelie\libs\pyglet\player.py -> Changes: line 171, line 172.
+
+        '''
+
+        player.delete()
+        
+        remove(file)
+
+    exit(0)
