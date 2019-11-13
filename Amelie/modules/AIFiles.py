cc9efb25d5b54367222c7f6070cc67e0d9282908
AmeliePick <ameliepickdev@gmail.com>
2019-11-13 13:54:33 +0300

    Add descriptions for fields

--- Amelie/modules/AIFiles.py
+++ Amelie/modules/AIFiles.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 
+# Storage for DB lines of stop words 
 clearSearch = list()
+
+# Storage for DB lines
 dataSet = list()
+
+# Storage for DB answer lines
 ANfile = list()
+
+# Storages current language choice
 checkLang = ""
