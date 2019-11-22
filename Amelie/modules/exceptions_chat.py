59741720bca41bc21ea0ca32dca4d62ce64d141c
AmeliePick <ameliepickdev@gmail.com>
2019-11-22 19:03:14 +0300

    fix program adding

--- Amelie/modules/exceptions_chat.py
+++ Amelie/modules/exceptions_chat.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from libs.configParser import Print
+from libs.configParser import DisplayText
 
 '''
 Request module for adding a program to the database
@@ -7,14 +7,56 @@ Request module for adding a program to the database
 '''
 
 
+
+
+
+def AddProgramm():
+    while(True):
+        rowInput_ = input("\nWrite break that would leave \nThe addition is as follows: program Name = Path\n\nEnter: ")
+        if rowInput_ == "Break" or  rowInput_ == "break":
+            print("Exiting...")
+                
+            return 0 
+
+        else:
+            # If program name is in file already, that will be replace this line with new input of user
+
+            input_ = rowInput_.split(" = ")
+            lines = list()
+            editInFile = False
+
+            with open('../DataBase/added_programms.json', 'r') as file_read:
+                for fileLine in file_read:
+                    rowLine = fileLine.replace('\n', '')
+                    fileLine = fileLine.split(" = ")
+                    
+                    if fileLine[0] == input_[0]:
+                        rowLine = fileLine[0] + " = " + input_[1]
+                        lines.append(rowLine)
+
+                        editInFile = True
+                        break
+                        
+                    else:
+                        lines.append(rowLine)
+                        
+
+            if editInFile == False:
+                lines.append(rowInput_)
+
+            with open('../DataBase/added_programms.json', 'w') as file_write:
+                for line in lines:
+                    file_write.write(line + '\n')
+
+    return
+
+
 def programmNotFound():
-    print("<--- "+ Print("n_found"))
-    print(Print("addPr"))
-    AddProgramm = input("--> ")
+    print("<--- "+ DisplayText.print("n_found"))
+    print(DisplayText.print("addPr"))
+    input_ = input("--> ")
 
-    if AddProgramm == "Y" or  AddProgramm == "y":
-        from .AddPr import AddProgramm
-
+    if input_ == "Y" or  input_ == "y":
         AddProgramm()
     else:
         return 0
