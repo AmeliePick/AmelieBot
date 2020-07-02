81835eb5bed47ca193d11f6a333db4b247ff63f5
AmeliePick <ameliepickdev@gmail.com>
2020-07-02 20:50:55 +0300

    refactor(cli/modules/tools/configParser.py, dialog.py)     - Remake "MessagePrinter" class.     - Move "MessagePrinter" to another module and rename the class.

---
+++ src/cli/Amelie/modules/chat/dialog.py
@@ -0,0 +1,65 @@
+class Dialog():
+    """ Printing messages in current language
+
+    The class is a singleton.
+
+    serviceExpressions: the data from the data base file.
+
+    """
+
+
+
+    serviceExpressions: str
+
+
+
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(Dialog, cls).__new__(cls)
+            return cls.instance
+            
+        return cls.instance
+
+
+
+    def __init__(self, languageValue: str):
+        super().__init__()
+
+        initDataBase(languageValue)
+
+        return
+    
+
+    def getMessageBy(self, value: str) -> str:
+        ''' Getting the text of expression by given value
+
+        value: name of the expression.
+
+        '''
+
+        text = []
+        for line in self.serviceExpressions:
+            row = line.split(' # ')
+
+            if row[0] == value:
+                text.append(row[1])
+                return ''.join(text)
+
+
+        '''
+        If the error exist in the name of the expression
+        By recursion it finds the value of the expression by "error" tag
+        '''
+        return self.Print("error")
+
+
+
+    def initByDataBase(self, appLanguage: str):
+        if languageValue == "RU":
+            with open("../DataBase/Service_expressionsRU.json", encoding='utf-8') as file:
+                self.serviceExpressions = file.readlines()
+
+        elif languageValue == "EN":
+            with open("../DataBase/Service_expressionsEN.json", encoding='utf-8') as file:
+                self.serviceExpressions = file.readlines()
+        return
---
+++ src/cli/Amelie/modules/tools/configParser.py
@@ -0,0 +1,4 @@
+Unstaged changes after reset:
+D	src/Model/Amelie/modules/chat/AICore.py
+D	src/Model/Amelie/modules/tools/configParser.py
+M	src/cli/Amelie/modules/tools/configParser.py
