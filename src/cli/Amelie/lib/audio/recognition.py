e47b962556237d4d7661d2685bef604a2b574cb4
AmeliePick <ameliepickdev@gmail.com>
2020-07-13 20:24:58 +0300

    refactor(lib/audio/recognition.py) Refactoring.

--- src/cli/Amelie/lib/audio/recognition.py
+++ src/cli/Amelie/lib/audio/recognition.py
@@ -1,16 +1,9 @@
 # -*- coding: utf-8 -*
 
-''' Recognizer module
-Listens to speech, converts to text and sends it to the chat module for processing.
-'''
 
 
 import pyaudio, wave
 import speech_recognition as sr
-
-from os                     import system
-from .configParser          import SettingsControl, DisplayText
-from modules.AudioManagement   import *
 
 
 
@@ -18,54 +11,38 @@ class SpeechRecognition():
     ''' Recognize the speech and convert it in the text.
     '''
 
-    def __init__(self):
-        # Microphone and Recognition
-        self.r = sr.Recognizer()
+
+    lang: str
+    recognizer: sr.Recognizer
+    micro: sr.Microphone
+
+
+
+    def __init__(self, appLanguage: str):
+        self.lang = appLanguage
+        self.recognizer = sr.Recognizer()
         self.micro = sr.Microphone()
+
 
 
     def calibration(self) -> None:
          ''' Noise calibration.
          '''
 
-         print(DisplayText.print("Silence"))
-
-         try:
-             with self.micro as source:
-                self.r.adjust_for_ambient_noise(source)
-         except OSError:
-            print(DisplayText.print("microAccesDenied"))
-            system("pause")
-            calibration()
+         with self.micro as source:
+            self.recognizer.adjust_for_ambient_noise(source)
          return
     
 
+
     def recognize(self) -> str:
-        while(True):
-            # Listening to the microphone
-            playAudio('../Res/Sounds/readytohear.wav')
-            print(DisplayText.print("SaySTH"))
+        audio = sr.AudioData()
 
-            with self.micro as source:
-                audio = self.r.listen(source)
+        # Listening to the microphone
+        with self.micro as source:
+            audio = self.recognizer.listen(source)
 
-            # Speech to text recording
-            try:
-                input_ = self.r.recognize_google(audio, language=SettingsControl.getConfig("Settings", "lang"))
-                print("---> ", input_)
-        
-                return input_
+        # Speech to text
+        return self.recognizer.recognize_google(audio, language = self.lang)
 
-            except sr.UnknownValueError:
-                print(DisplayText.print("errSay"))
-                system("pause")
-                continue
-
-            except sr.RequestError:
-                raise ConnectionError 
-
-            return
-
-
-
-speechRecognition = SpeechRecognition()
+        return
