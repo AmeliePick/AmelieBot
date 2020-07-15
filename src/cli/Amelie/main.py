ba1fc567c71ff783a71b3081094f2b690e06da2f
AmeliePick <ameliepickdev@gmail.com>
2020-07-15 23:35:52 +0300

    refactor(cli/Amelie/main.py) Rewrite main.

--- src/cli/Amelie/main.py
+++ src/cli/Amelie/main.py
@@ -6,180 +6,81 @@
 # ======================= #
 
 
-''' The main file of the bot. 
-The functions of entering the program and choosing the bot mode are called.
-'''
 
-from time   import sleep
-from sys    import stdin, exit as sys_exit
-from sys    import argv
-from sys    import exc_info 
-from sys    import executable as exe
-from os     import path as os_path
-from os     import execl
-from os     import _exit
-from os     import remove
+from os                 import path as os_path
+from memory_profiler    import memory_usage
 
-from memory_profiler import memory_usage
+from Amelie    import Amelie
+from cli       import Console
 
-
-from modules.tools.configParser          import SettingsControl, DisplayText
-from modules.tools.sessionLog            import sessionLogger, LogWrite
-from modules.update                      import checkUpdate, download
-
-
-
-def restart() -> None:
-    #restart of the bot
-    execl(exe, exe, *argv)
-
-
-def getUpdate() -> None:
-    # --- Check updates ---
-    isupdate  = checkUpdate()
-    if isupdate:
-        print(DisplayText.print("isUpdate"))
-        sleep(1.5)
-
-        download(isupdate)
-        restart()
-
-    else:
-        print(DisplayText.print("istUpdate"), '\n')
-
-    #TODO: remove here
-    
-
-    print("AmelieBot " + SettingsControl.getConfig("Settings", "ver"), '\n')
-
-
-def set_username() -> None:
-    if os_path.exists("../DataBase/username.json"):
-        username = ''
-        with open("../DataBase/username.json", 'r') as file_user:
-            username = file_user.read()
-
-        if (username == ""):
-            print(DisplayText.print("getName"))
-            username = input("> ")
-
-        if (username != ""):
-            with open("../DataBase/username.json", "w") as writeUsername:
-                writeUsername.write(username)
-            print ("\n Welcome, "+ username + "!")
-
-        else:
-            print (DisplayText.print("emptyName"))
-            set_username()
-
-    else:
-        with open("../DataBase/username.json", 'w') as file_user:
-            file_user.close()
-        set_username()
-
-maxRAMUsage = memory_usage()
-def RAMCheck() -> None:
-    global maxRAMUsage
-    if(memory_usage() > maxRAMUsage):
-        maxRAMUsage = memory_usage()
-    return
 
 
 def main() -> int:
-     # ----- Start the bot -----
-    global stopWatch
-    global sessionLogger
+    AmelieInstance = Amelie()
+    console = Console()
 
-    print ( 70 * "_")
-    print ("\t\t                         _ _       \n" +
-           "\t\t    /\                  | (_)      \n" +
-           "\t\t   /  \   _ __ ___   ___| |_  ___  \n" +
-           "\t\t  / /\ \ | '_ ` _ \ / _ \ | |/ _ \ \n" +
-           "\t\t / ____ \| | | | | |  __/ | |  __/ \n" +
-           "\t\t/_/    \_\_| |_| |_|\___|_|_|\___| ")
-    print ( 70 * "_")
 
-    getUpdate()
-    set_username()
-    
-    
-    
+    username = str(AmelieInstance.getUsername())
 
-   
 
-    print(DisplayText.print("Learning"))
-    from modules.chat.AICore    import Chat
+    console.printLogo()
 
-    chat = Chat()
-
-    # inline function
-    
-
-    # --- Chat ---
-    print(DisplayText.print("Voice_control"))
-    On = input ("--> ")
+    console.writeLine(AmelieInstance.getMessageFor("Voice_control"))
+    turnOnTheVoice = console.readLine("--> ")
     while (True):
         try:
-            if On == "Y" or On ==  "y":
-                from modules.recognition   import speechRecognition
-
-                if SettingsControl.getConfig("Settings", "lang") == "RU":
-                    from modules.RuSpeak import speak
-
-                    speechRecognition.calibration()
-                    while (True):
-                        speak(chat.launch(speechRecognition.recognize()))
-                        RAMCheck()
-                        continue
-                else:
-                    from modules.Speak import speak
-
-                    speechRecognition.calibration()
-                    while (True):
-                        speak(chat.launch(speechRecognition.recognize()))
-                        RAMCheck()
-                        continue
-
-            elif On == "N" or On ==  "n":
+            if turnOnTheVoice == "Y" or turnOnTheVoice ==  "y":
                 while(True):
-                    chat.launch()
-                    if chat.getCode() == 0:
-                        sleep(1)
-                        
+                    userInput = console.readLine('\n' + username + ": ")
+                    chatAnswer = AmelieInstance.speechChat(userInput)
 
-                        _exit(0)          
+                    console.writeLine("\t\tAmelie: " + chatAnswer)
+
+                    AmelieInstance.isExit()
+                
+
+            elif turnOnTheVoice == "N" or turnOnTheVoice ==  "n":
+                while(True):
+                    userInput = console.readLine('\n' + username + ": ")
+                    chatAnswer = AmelieInstance.silentChat(userInput)
+
+                    console.writeLine("\n\t\t\tAmelie: " + chatAnswer)
+
+                    AmelieInstance.isExit()
+
+                                  
             else:
-                print(DisplayText.print("WrongInput"))
-                On = input("--> ")
+                console.writeLine(AmelieInstance.getMessageFor("WrongInput"))
+                turnTheVoice = console.readLine("--> ")
                 continue
 
 
         except MemoryError:
-            print(DisplayText.print("error"))
+            console.writeLine(AmelieInstance.getMessageFor("error"))
             continue
 
         except SystemExit:
-            sessionLogger.sessionCollector( "max RAM usage", str(maxRAMUsage) )
-            _exit(0)
+            
+
+            break
 
         except ConnectionError:
-            print(DisplayText.print("service_error"))
+            console.writeLine(AmelieInstance.getMessageFor("service_error"))
       
         except OSError:
-            LogWrite()
-            print(DisplayText.print("error"))
+            AmelieInstance.writeLog()
+            console.writeLine(AmelieInstance.getMessageFor("error"))
 
         except Exception:
-            LogWrite()
-            print(DisplayText.print("crash"))
+            AmelieInstance.writeLog()
+            console.writeLine(AmelieInstance.getMessageFor("crash"))
 
-            restart()
+            AmelieInstance.restart()
 
 
-        print(DisplayText.print("Voice_control"))
-        On = input ("--> ")
 
     return 0
+
 
 
 if __name__ == '__main__':
