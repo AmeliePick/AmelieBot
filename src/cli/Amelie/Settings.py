95a6a3acf3d4482a5ab5e15a6bf91faaef2d70b3
AmeliePick <ameliepickdev@gmail.com>
2020-07-20 17:07:30 +0300

    feature(cli/Amelie/main.py, Settings.py)     Add the setting of user's name and language to the config file.

---
+++ src/cli/Amelie/Settings.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+
+
+
+from lib.tools.system          import FileManager
+from lib.tools.iniParser       import IniParser
+
+
+
+class Settings(object):
+    """Control the config file"""
+
+
+    _fileManager: FileManager
+    _iniParser: IniParser
+    _methods: dict
+
+    _supportingLangs: dict
+
+
+    def __init__(self):
+        super().__init__()
+        self._iniParser = IniParser("settings.ini")
+
+        self._methods = dict()
+        self._supportingLangs = dict()
+
+        langs = (self._iniParser.getValue("AppData", "langs", "../../Version/AppSetup.ini")).split(", ")
+        for index in range(0, len(langs)):
+            self._supportingLangs[index + 1] = langs[index]
+
+
+
+        if len(FileManager.readFile("settings.ini")) == 0:
+            self._iniParser.setSection("Settings")
+            self._iniParser.setSection("User")
+
+            self._iniParser.setValue("Settings", 
+                                     "ver", 
+                                     self._iniParser.getValue("Setup", "Version", "../../Version/AppSetup.ini")
+                                    )
+
+
+            
+
+
+        if len(self.getLanguage()) != 2:
+            self._iniParser.setValue("Settings", "lang", "-")
+            self._methods["lang"] = self.setLanguage
+
+
+        from re import sub
+        if len(sub('[\t, \n, \r \s]', '', self.getUsername())) < 2:
+            self._iniParser.setValue("User", "name", '')
+            self._methods["username"] = self.setUsername
+
+
+        return
+
+
+
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(Settings, cls).__new__(cls)
+            return cls.instance
+            
+        return cls.instance
+
+
+
+
+    def setLanguage(self, langValue: str) -> None:
+        self._iniParser.setValue("Settings", "lang", langValue)
+
+        return
+
+
+
+    def setUsername(self, nameValue: str) -> None:
+        self._iniParser.setValue("User", "name", nameValue)
+
+        return
+
+
+
+    def getLanguage(self) -> str:
+        return self._iniParser.getValue("Settings", "lang")
+
+
+
+    def getSupportingLangs(self) -> dict:
+        return self._supportingLangs
+
+
+
+    def getUsername(self) -> str:
+        return self._iniParser.getValue("User", "name")
+
+
+
+    def getMethodsToResolveErrors(self) -> dict:
+        return self._methods
--- src/cli/Amelie/main.py
+++ src/cli/Amelie/main.py
@@ -11,23 +11,68 @@ from os                 import path as os_path
 from threading          import Thread
 from memory_profiler    import memory_usage
 
-
+from lib.chat.dialog import Dialog  
+from Settings  import Settings
 from Amelie    import Amelie
 from cli       import Console
 
 
 
 def main() -> int:
-    AmelieInstance = Amelie()
     console = Console()
-
-
-    username = str(AmelieInstance.getUsername())
-
-
     console.printLogo()
 
-    console.writeLine(AmelieInstance.getMessageFor("Voice_control"))
+
+
+    settingsFile = Settings()
+    dialog = Dialog('en') if settingsFile.getLanguage() == '-' else Dialog(settingsFile.getLanguage())
+        
+    #---------------------------------------------------------------------------------------------------------------#
+    #                                   FILLING THE SETTINGS FILE WITH USER INPUT                                   #
+
+    settingsMethods = settingsFile.getMethodsToResolveErrors()
+    for methodname, method in settingsMethods.items():
+        if methodname == "lang":
+            console.write(dialog.getMessageFor("Choose_lang"))
+
+            langs = settingsFile.getSupportingLangs()
+            for key, value in langs.items():
+                console.write('[' + str(key) + ']')
+                console.write(value.upper() + ' ')
+            console.write('\n')
+
+            while(True):
+                langInput = int(console.readLine("\n--> "))
+
+                if langInput in langs.keys():
+                    method(langs[langInput])
+                    break
+
+                else:
+                    console.writeLine(dialog.getMessageFor("WrongInput"))
+
+
+            dialog.changeLanguage(settingsFile.getLanguage())
+
+        elif methodname == "username":
+            while(True):
+                console.writeLine(dialog.getMessageFor("getName"))
+                usernameInput = (console.readLine("\n--> "))
+
+                from re import sub
+                if len(sub('[\t, \n, \r \s]', '', usernameInput)) >= 2:
+                    method(usernameInput)
+                    break
+
+    #---------------------------------------------------------------------------------------------------------------#
+
+
+    AmelieInstance = Amelie()
+    
+    username = str(settingsFile.getUsername())
+
+    
+    console.writeLine('\n' + dialog.getMessageFor("Voice_control"))
     turnOnTheVoice = console.readLine("--> ")
     while (True):
         try:
@@ -37,7 +82,7 @@ def main() -> int:
                         AmelieInstance.recorderCalibration()
                     except OSError:
                         from os import system
-                        console.writeLine(AmelieInstance.getMessageFor("microAccesDenied"))
+                        console.writeLine(dialog.getMessageFor("microAccesDenied"))
                         system("pause")
                         continue
 
@@ -62,7 +107,7 @@ def main() -> int:
                      
 
             else:
-                console.writeLine(AmelieInstance.getMessageFor("WrongInput"))
+                console.writeLine(dialog.getMessageFor("WrongInput"))
                 turnOnTheVoice = console.readLine("--> ")
                 continue
 
@@ -73,7 +118,7 @@ def main() -> int:
 
         except MemoryError:
             AmelieInstance.writeLog()
-            console.writeLine(AmelieInstance.getMessageFor("error"))
+            console.writeLine(dialog.getMessageFor("error"))
             continue
 
         except SystemExit:
@@ -83,15 +128,15 @@ def main() -> int:
 
         except ConnectionError:
             AmelieInstance.writeLog()
-            console.writeLine(AmelieInstance.getMessageFor("service_error"))
+            console.writeLine(dialog.getMessageFor("service_error"))
       
         except OSError:
             AmelieInstance.writeLog()
-            console.writeLine(AmelieInstance.getMessageFor("error"))
+            console.writeLine(dialog.getMessageFor("error"))
 
         except Exception:
             AmelieInstance.writeLog()
-            console.writeLine(AmelieInstance.getMessageFor("crash"))
+            console.writeLine(dialog.getMessageFor("crash"))
 
             AmelieInstance.restart()
 
