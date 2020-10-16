44b1c3e1935da9bfaad81db0de8523a6377be4c3
AmeliePick <ameliepickdev@gmail.com>
2020-10-16 17:55:45 +0300

    chore(src/gui/src/core/amelie, main) Add methods.

--- src/gui/src/core/amelie.cpp
+++ src/gui/src/core/amelie.cpp
@@ -19,20 +19,25 @@ void AmelieApplication::initChatBot()
     this->dialog = Dialog::getInstance(settings->getLanguage());
 }
 
+void AmelieApplication::changeInputMode(bool enableVoice)
+{
+
+}
+
 
 
 int AmelieApplication::main(int argc, char** argv)
 {
     GUI gui(argc, argv);
 
-    // Filling the settings file
+    /* Filling the settings file
     while(settings->getLanguage() == "-" || settings->getUsername() == "")
     {
         gui.showSettingsWindow(settings);
     }
 
-    initChatBot();
-    return gui.showMainWindow(chat, dialog);
+    initChatBot();*/
+    return gui.showSettingsWindow(settings);
 }
 
 #pragma endregion
@@ -43,7 +48,7 @@ int AmelieApplication::main(int argc, char** argv)
 
 
 
-#pragma region Amelie
+#pragma region Chat
 AmelieApplication::Chat::Chat(const char* appLanguage)
 {
     this->classInstance = AmelieCreateInstance(appLanguage);
@@ -125,5 +130,182 @@ const char* AmelieApplication::Chat::getUserInput()
 AmelieApplication::Chat::~Chat()
 {
     AmelieDelete(classInstance);
+}
+#pragma endregion
+
+
+
+
+
+#pragma region FileManager
+AmelieApplication::FileManager* AmelieApplication::FileManager::getInstance()
+{
+    return nullptr;
+}
+
+
+
+bool AmelieApplication::FileManager::fileExist(const char* file)
+{
+    return false;
+}
+
+
+
+void AmelieApplication::FileManager::writeToFile(const char* value, const char* file, const char* mode, const char* _encoding)
+{
+
+}
+
+
+
+const char * AmelieApplication::FileManager::readFile(const char* file, const char* _encoding)
+{
+    return nullptr;
+}
+
+void AmelieApplication::FileManager::createFile(const char* file)
+{
+
+}
+
+
+
+void AmelieApplication::FileManager::deleteFile(const char* file)
+{
+
+}
+
+
+
+void AmelieApplication::FileManager::clearFile(const char* file)
+{
+
+}
+
+
+
+AmelieApplication::FileManager::~FileManager()
+{
+
+}
+#pragma endregion
+
+
+
+
+
+#pragma region Settings
+AmelieApplication::Settings* AmelieApplication::Settings::getInstance()
+{
+    return nullptr;
+}
+
+
+
+void AmelieApplication::Settings::setLanguage(const char* langValue)
+{
+
+}
+
+
+
+void AmelieApplication::Settings::setUsername(const char* nameValue)
+{
+
+}
+
+
+
+const char* AmelieApplication::Settings::getLanguage()
+{
+    return nullptr;
+}
+
+
+
+std::multimap<int, const char*> AmelieApplication::Settings::getSupportingLangs()
+{
+    return std::multimap<int, const char*>();
+}
+
+
+
+const char* AmelieApplication::Settings::getUsername()
+{
+    return nullptr;
+}
+
+
+
+std::multimap<const char*, void*> AmelieApplication::Settings::getMethodsToResolveErrors()
+{
+    return std::multimap<const char*, void*>();
+}
+#pragma endregion
+
+
+
+
+
+#pragma region Network
+AmelieApplication::Network* AmelieApplication::Network::getInstance()
+{
+    return nullptr;
+}
+
+
+
+bool AmelieApplication::Network::checkNetworkConnection()
+{
+    return false;
+}
+#pragma endregion
+
+
+
+
+
+#pragma region Logger
+AmelieApplication::Logger* AmelieApplication::Logger::getInstance()
+{
+    return nullptr;
+}
+
+
+
+void AmelieApplication::Logger::addRecord(const char* recordTitle, const char* value)
+{
+
+}
+
+
+
+void AmelieApplication::Logger::logWrite()
+{
+
+}
+#pragma endregion
+
+
+
+
+
+#pragma region Dialog
+AmelieApplication::Dialog* AmelieApplication::Dialog::getInstance(const char* appLanguage)
+{
+    return nullptr;
+}
+
+
+const char* AmelieApplication::Dialog::getMessageFor(const char* expression)
+{
+    return nullptr;
+}
+
+
+void AmelieApplication::Dialog::changeLanguage(const char* appLanguage)
+{
+
 }
 #pragma endregion
--- src/gui/src/core/main.cpp
+++ src/gui/src/core/main.cpp
@@ -1,16 +1,8 @@
-#include <QGuiApplication>
-#include <QQmlApplicationEngine>
-#include <QIcon>
+#include "amelie.h"
+
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-
-    QGuiApplication app(argc, argv);
-    app.setWindowIcon(QIcon("../../../resources/AppIcon/icon.ico"));
-
-    QQmlApplicationEngine engine;
-    engine.load(QUrl::fromLocalFile("../src/view/main.qml"));
-
-    return app.exec();
+    AmelieApplication amelie;
+    return amelie.main(argc, argv);
 }
